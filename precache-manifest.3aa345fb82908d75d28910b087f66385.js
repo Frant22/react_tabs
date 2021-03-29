46d98b02fc7c9bfb60e60da93614bbc84432e1d7
Vladyslav Semyrekov <semyrekov.vladyslav@gmail.com>
2021-03-29 17:34:11 +0300

    Updates

---
+++ precache-manifest.3aa345fb82908d75d28910b087f66385.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2b3786790bcad425b70ba187e72ef4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a8539af5722f5242996d",
+    "url": "./static/css/main.b6037047.chunk.css"
+  },
+  {
+    "revision": "9a516c52589114fdec1a",
+    "url": "./static/js/2.1656df69.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.1656df69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8539af5722f5242996d",
+    "url": "./static/js/main.8c5b7dd9.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  },
+  {
+    "revision": "201b8623f5e980c27cb7f415f273559f",
+    "url": "./static/media/t-rex.201b8623.png"
+  },
+  {
+    "revision": "e2f386ec6d70166fa66fe1299c17a61d",
+    "url": "./static/media/titanosaurus.e2f386ec.png"
+  },
+  {
+    "revision": "db648f6a4723b0839204e40b09eb593e",
+    "url": "./static/media/triceratops.db648f6a.png"
+  }
+]);
